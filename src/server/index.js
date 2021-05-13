f36aed261c48b4e1aed91d3a261cce5c7a887f46
b-duffy <45366900+b-duffy@users.noreply.github.com>
2021-05-13 15:32:09 -0500

    fix: adjusting entry path

--- src/server/index.js
+++ src/server/index.js
@@ -7,20 +7,31 @@ const mockAPIResponse = require('./mockAPI.js')
 
 const app = express()
 
+/* Dependencies */
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require('cors');
+const { RSA_NO_PADDING } = require('constants');
+app.use(cors());
+
 app.use(express.static('dist'))
 
 console.log(__dirname)
-
-app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
-})
 
 // designates what port the app will listen to for incoming requests
 app.listen(8000, function () {
     console.log('Example app listening on port 8000!')
 })
 
+app.get('/', function (req, res) {
+    // res.sendFile('dist/index.html')
+    res.sendFile(path.resolve('src/client/views/index.html'))
+})
+
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
 })
+
