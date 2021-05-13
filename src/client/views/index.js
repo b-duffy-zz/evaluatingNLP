4805a82637c99c31b61c57634f524824cafcb622
b-duffy <45366900+b-duffy@users.noreply.github.com>
2021-05-13 16:15:39 -0500

    saving

---
+++ src/client/views/index.js
@@ -0,0 +1,13 @@
+import { checkForName } from '../js/nameChecker'
+import { handleSubmit } from '../js/formHandler'
+
+import '../styles/main.scss'
+
+console.log(checkForName);
+
+alert("I EXIST")
+
+export {
+    checkForName,
+    handleSubmit
+}
