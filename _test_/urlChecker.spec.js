2f1bfbe4152cfe45c2482a2e7c0aa828921fe52e
br3nd4nduffy <76970152+br3nd4nduffy@users.noreply.github.com>
2021-05-18 15:29:45 -0500

    feat: adding test for urlChecker

---
+++ _test_/urlChecker.spec.js
@@ -0,0 +1,7 @@
+import { urlChecker } from '../src/client/js/urlChecker'
+
+describe("Testing the URl validation  functionality", () => {
+    test("Testing the urlChecker() function", () => {
+        expect(urlChecker).toBeDefined();
+    }
+    )})
